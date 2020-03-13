236eb40f408bf486aa22a42dbe7264070afe8c68
Felicity <FelicityRayner@protonmail.com>
2020-03-13 11:18:08 +0100

    deleted block.go

--- models/block.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Block struct {
-	transactions      []string
-	previousBlockHash string
-	receiver_address  string
-}
--- models/ledger.go
+++ models/ledger.go
@@ -1,7 +1,7 @@
 package models
 
-import block;
+import Block;
 
 type Ledger struct {
-	blocks array[Block]
+	blocks []Block
 }
